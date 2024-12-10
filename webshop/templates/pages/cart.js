840f840337b14c2e0adf44a63b46e7f467ade8d1
Marton <antal.marci@gmail.com>
2024-12-10 19:16:52 +0000

    Add translation and formatting

--- webshop/templates/pages/cart.js
+++ webshop/templates/pages/cart.js
@@ -6,12 +6,12 @@ frappe.provide("webshop.webshop.shopping_cart");
 var shopping_cart = webshop.webshop.shopping_cart;
 
 $.extend(shopping_cart, {
-	show_error: function(title, text) {
+	show_error: function (title, text) {
 		$("#cart-container").html('<div class="msg-box"><h4>' +
 			title + '</h4><p class="text-muted">' + text + '</p></div>');
 	},
 
-	bind_events: function() {
+	bind_events: function () {
 		shopping_cart.bind_place_order();
 		shopping_cart.bind_request_quotation();
 		shopping_cart.bind_change_qty();
@@ -21,31 +21,31 @@ $.extend(shopping_cart, {
 		shopping_cart.bind_custom_delivery_date();
 	},
 
-	bind_custom_delivery_date: function() {
-		$("#btn_custom_delivery_date").on("change", function() {
+	bind_custom_delivery_date: function () {
+		$("#btn_custom_delivery_date").on("change", function () {
 			var delivery_date = this.value
 			shopping_cart.set_delivery_date(this, delivery_date);
 		})
 	},
 
-	bind_place_order: function() {
-		$(".btn-place-order").on("click", function() {
+	bind_place_order: function () {
+		$(".btn-place-order").on("click", function () {
 			shopping_cart.place_order(this);
 		});
 	},
 
-	bind_request_quotation: function() {
-		$('.btn-request-for-quotation').on('click', function() {
+	bind_request_quotation: function () {
+		$('.btn-request-for-quotation').on('click', function () {
 			shopping_cart.request_quotation(this);
 		});
 	},
 
-	bind_change_qty: function() {
+	bind_change_qty: function () {
 		// bind update button
-		$(".cart-items").on("change", ".cart-qty", function() {
+		$(".cart-items").on("change", ".cart-qty", function () {
 			var item_code = $(this).attr("data-item-code");
 			var newVal = $(this).val();
-			shopping_cart.shopping_cart_update({item_code, qty: newVal});
+			shopping_cart.shopping_cart_update({ item_code, qty: newVal });
 		});
 
 		$(".cart-items").on('click', '.number-spinner button', function () {
@@ -73,8 +73,8 @@ $.extend(shopping_cart, {
 		});
 	},
 
-	bind_change_notes: function() {
-		$('.cart-items').on('change', 'textarea', function() {
+	bind_change_notes: function () {
+		$('.cart-items').on('change', 'textarea', function () {
 			const $textarea = $(this);
 			const item_code = $textarea.attr('data-item-code');
 			const qty = $textarea.closest('tr').find('.cart-qty').val();
@@ -87,7 +87,7 @@ $.extend(shopping_cart, {
 		});
 	},
 
-	bind_remove_cart_item: function() {
+	bind_remove_cart_item: function () {
 		$(".cart-items").on("click", ".remove-cart-item", (e) => {
 			const $remove_cart_item_btn = $(e.currentTarget);
 			var item_code = $remove_cart_item_btn.data("item-code");
@@ -99,20 +99,21 @@ $.extend(shopping_cart, {
 		});
 	},
 
-	render_tax_row: function($cart_taxes, doc, shipping_rules) {
+	render_tax_row: function ($cart_taxes, doc, shipping_rules) {
 		var shipping_selector;
-		if(shipping_rules) {
-			shipping_selector = '<select class="form-control">' + $.map(shipping_rules, function(rule) {
-				return '<option value="' + rule[0] + '">' + rule[1] + '</option>' }).join("\n") +
-			'</select>';
+		if (shipping_rules) {
+			shipping_selector = '<select class="form-control">' + $.map(shipping_rules, function (rule) {
+				return '<option value="' + rule[0] + '">' + rule[1] + '</option>'
+			}).join("\n") +
+				'</select>';
 		}
 
 		var $tax_row = $(repl('<div class="row">\
 			<div class="col-md-9 col-sm-9">\
 				<div class="row">\
 					<div class="col-md-9 col-md-offset-3">' +
-					(shipping_selector || '<p>%(description)s</p>') +
-					'</div>\
+			(shipping_selector || '<p>%(description)s</p>') +
+			'</div>\
 				</div>\
 			</div>\
 			<div class="col-md-3 col-sm-3 text-right">\
@@ -120,44 +121,44 @@ $.extend(shopping_cart, {
 			</div>\
 		</div>', doc)).appendTo($cart_taxes);
 
-		if(shipping_selector) {
-			$tax_row.find('select option').each(function(i, opt) {
-				if($(opt).html() == doc.description) {
+		if (shipping_selector) {
+			$tax_row.find('select option').each(function (i, opt) {
+				if ($(opt).html() == doc.description) {
 					$(opt).attr("selected", "selected");
 				}
 			});
-			$tax_row.find('select').on("change", function() {
+			$tax_row.find('select').on("change", function () {
 				shopping_cart.apply_shipping_rule($(this).val(), this);
 			});
 		}
 	},
 
-	apply_shipping_rule: function(rule, btn) {
+	apply_shipping_rule: function (rule, btn) {
 		return frappe.call({
 			btn: btn,
 			type: "POST",
 			method: "webshop.webshop.shopping_cart.cart.apply_shipping_rule",
 			args: { shipping_rule: rule },
-			callback: function(r) {
-				if(!r.exc) {
+			callback: function (r) {
+				if (!r.exc) {
 					shopping_cart.render(r.message);
 				}
 			}
 		});
 	},
 
-	place_order: function(btn) {
+	place_order: function (btn) {
 		shopping_cart.freeze();
 
 		return frappe.call({
 			type: "POST",
 			method: "webshop.webshop.shopping_cart.cart.place_order",
 			btn: btn,
-			callback: function(r) {
-				if(r.exc) {
+			callback: function (r) {
+				if (r.exc) {
 					shopping_cart.unfreeze();
 					var msg = "";
-					if(r._server_messages) {
+					if (r._server_messages) {
 						msg = JSON.parse(r._server_messages || []).join("<br>");
 					}
 
@@ -173,18 +174,18 @@ $.extend(shopping_cart, {
 		});
 	},
 
-	request_quotation: function(btn) {
+	request_quotation: function (btn) {
 		shopping_cart.freeze();
 
 		return frappe.call({
 			type: "POST",
 			method: "webshop.webshop.shopping_cart.cart.request_for_quotation",
 			btn: btn,
-			callback: function(r) {
-				if(r.exc) {
+			callback: function (r) {
+				if (r.exc) {
 					shopping_cart.unfreeze();
 					var msg = "";
-					if(r._server_messages) {
+					if (r._server_messages) {
 						msg = JSON.parse(r._server_messages || []).join("<br>");
 					}
 
@@ -200,19 +201,19 @@ $.extend(shopping_cart, {
 		});
 	},
 
-	set_delivery_date: function(btn, deliv_date) {
+	set_delivery_date: function (btn, deliv_date) {
 		shopping_cart.freeze();
 
 		return frappe.call({
 			type: "POST",
 			method: "webshop.webshop.shopping_cart.cart.set_delivery_date",
 			btn: btn,
-			args: {delivery_date: deliv_date},
-			callback: function(r) {
+			args: { delivery_date: deliv_date },
+			callback: function (r) {
 				shopping_cart.unfreeze();
-				if(r.exc) {
+				if (r.exc) {
 					var msg = "";
-					if(r._server_messages) {
+					if (r._server_messages) {
 						msg = JSON.parse(r._server_messages || []).join("<br>");
 					}
 
@@ -220,41 +221,40 @@ $.extend(shopping_cart, {
 						.empty()
 						.html(msg || frappe._("Something went wrong!"))
 						.toggle(true);
-				} 
+				}
 			}
 		});
 	},
 
-	bind_coupon_code: function() {
-		$(".bt-coupon").on("click", function() {
+	bind_coupon_code: function () {
+		$(".bt-coupon").on("click", function () {
 			shopping_cart.apply_coupon_code(this);
 		});
 	},
 
-	apply_coupon_code: function(btn) {
+	apply_coupon_code: function (btn) {
 		return frappe.call({
 			type: "POST",
 			method: "webshop.webshop.shopping_cart.cart.apply_coupon_code",
 			btn: btn,
-			args : {
-				applied_code : $('.txtcoupon').val(),
+			args: {
+				applied_code: $('.txtcoupon').val(),
 				applied_referral_sales_partner: $('.txtreferral_sales_partner').val()
 			},
-			callback: function(r) {
-				if (r && r.message){
+			callback: function (r) {
+				if (r && r.message) {
 					location.reload();
 				}
 			}
 		});
 	},
 
-	filter_delivery_dates: function()
-	{
+	filter_delivery_dates: function () {
 		// Everything except weekend days
 		const validate = dateString => {
 			const day = (new Date(dateString)).getDay();
-			if (day==6 || day==7) {
-			return false;
+			if (day == 6 || day == 7) {
+				return false;
 			}
 			return true;
 		}
@@ -262,17 +262,15 @@ $.extend(shopping_cart, {
 		var today = new Date().toISOString().split('T')[0];
 		var datePicker = $("#btn_custom_delivery_date");
 		$("#btn_custom_delivery_date")[0].min = today
-		console.log("datePicker: ", $("#btn_custom_delivery_date")[0]);
 		$("#btn_custom_delivery_date")[0].onchange = evt => {
 			if (!validate(evt.target.value)) {
 				evt.target.value = '';
 			}
 		}
-		console.log("datePicker2: ", $("#btn_custom_delivery_date")[0]);
 	}
 });
 
-frappe.ready(function() {
+frappe.ready(function () {
 	if (window.location.pathname === "/cart") {
 		$(".cart-icon").hide();
 	}
